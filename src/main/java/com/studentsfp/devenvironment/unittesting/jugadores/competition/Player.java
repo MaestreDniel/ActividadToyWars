492f84051cc2bc4b123d4dc10c6440fcfc666011
Daniel Maestre <dmaestre@cifpfbmoll.eu>
2021-01-15 20:34:36 +0100

    15-01

---
+++ src/main/java/com/studentsfp/devenvironment/unittesting/jugadores/competition/Player.java
@@ -0,0 +1,7 @@
+package com.studentsfp.devenvironment.unittesting.jugadores.competition;
+
+import com.studentsfp.devenvironment.unittesting.p1.data.Person;
+
+public class Player extends Person {
+
+}
---
+++ src/main/java/com/studentsfp/devenvironment/unittesting/p2/data/Helper.java
@@ -0,0 +1,4 @@
+package com.studentsfp.devenvironment.unittesting.p2.data;
+
+public class Helper {
+}
