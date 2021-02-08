3b549d36407b284e8c493d852fac9ed542920187
Daniel Maestre <dmaestre@cifpfbmoll.eu>
2021-02-08 20:25:48 +0100

    Revert "feat: bank pojos"
    This reverts commit b2013fca

--- src/test/java/com/studentsfp/devenvironment/unittesting/UnittestingApplicationTests.java
+++ src/test/java/com/studentsfp/devenvironment/unittesting/UnittestingApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
 
 @SpringBootTest
-class UnitTestingApplicationTests {
+class UnittestingApplicationTests {
 
     @Test
     void contextLoads() {
