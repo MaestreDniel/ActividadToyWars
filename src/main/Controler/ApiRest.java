a9d0a0a7ac0c8791b5e28ea8f9774100adadcc0f
david <dgonzalezl@cifpfbmoll.eu>
2021-02-22 11:55:23 +0100

    Api rest

--- src/main/Controler/ApiRest.java
+++ src/main/Controler/ApiRest.java
@@ -3,8 +3,28 @@ package Controler;
 @RestController
 
 public class ApiRest {
-    /*@GetMapping("/Rest/Do")
+    @GetMapping("/Rest/Do")
     public Actions getCurrentActions(@RequestParam(value = "Actions", String Actions){
-        return new Actions(getCurrentActions())
-    }*/
+        return new Actions(getCurrentActions().JSON )
+    }
+
+@GetMapping ("/Rest/getCurrentStatus ")
+public Status getCurrentStatus(@RequestParam(value=Status),String, Status){
+    return getCurrentStatus();
+}
+@GetMapping("rest/render")
+public Render(RenderType.JSON){
+    return RenderType;
+}
+
+@GetMapping ("rest/new")
+public lifeBeing lifeBeing (@RequestParam value="Pokemon"){
+    return LifeBeing;
+}
+
+@GetMapping("rest/Get/Actions")
+    public UserActipm (@RequestParam valuue=getCurrentactions){
+        return;Iactions;
+    }
+
 }
